7065bb7c420e92c3970ebf88a8061c90cec41e7c
Stephen Anwar <stephen.anwar@gmail.com>
2020-08-02 21:39:51 +1000

    Allowed CORS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import serverless from 'serverless-http'
 import express from 'express'
+import cors from 'cors'
 import { graphqlHTTP } from 'express-graphql'
 import { root, gqlSchema } from './controllers/graphql.controller'
 
 const app = express()
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/api/v1/graphql', graphqlHTTP({ schema: gqlSchema, rootValue: root, graphiql: true }))
